3e6c8d6b597c5cf8810272525e7593fea6df821c
Daniel Newton <djpnewton@gmail.com>
2019-11-20 14:03:11 +1300

    add flask admin to project

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 import os
 import logging
-import time
 import sys
 
-from flask import Flask, request, jsonify, abort
+from flask import url_for, redirect, render_template, request, abort, jsonify
+from flask_security.utils import encrypt_password
 
-from database import db_session, init_db
-from models import ClaimCode
+from app_core import app, db
+from models import security, user_datastore, Role, User, ClaimCode
+import admin
 
-init_db()
 logger = logging.getLogger(__name__)
-app = Flask(__name__)
+
+#
+# Helper functions
+#
 
 def setup_logging(level):
     # setup logging
@@ -23,9 +26,39 @@ def setup_logging(level):
     # clear loggers set by any imported modules
     logging.getLogger().handlers.clear()
 
-@app.route("/")
-def hello():
-    return "boo"
+def add_user(email, password):
+    with app.app_context():
+        user = User.from_email(db.session, email)
+        if user:
+            logger.error('user already exists')
+            return
+        user = user_datastore.create_user(email=email, password=encrypt_password(password))
+        db.session.commit()
+
+def add_role(email, role_name):
+    with app.app_context():
+        user = User.from_email(db.session, email)
+        if not user:
+            logger.error('user does not exist')
+            return
+        role = Role.from_name(db.session, role_name)
+        if not role:
+            role = Role(name=role_name)
+            db.session.add(role)
+            user.roles.append(role)
+        elif role not in user.roles:
+            user.roles.append(role)
+        else:
+            logger.info('user already has role')
+        db.session.commit()
+
+#
+# Flask views
+#
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 #
 # Test
@@ -34,7 +67,7 @@ def hello():
 @app.route("/test/<token>")
 def test(token):
     #TODO: only allow when test environment var is present 
-    claim_code = ClaimCode.from_token(db_session, token)
+    claim_code = ClaimCode.from_token(db.session, token)
     if claim_code:
         return jsonify(claim_code.to_json())
     return abort(404)
@@ -49,8 +82,8 @@ def register():
     content = request.json
     token = content["token"]
     claim_code = ClaimCode(token)
-    db_session.add(claim_code)
-    db_session.commit()
+    db.session.add(claim_code)
+    db.session.commit()
     return jsonify(claim_code.to_json())
 
 @app.route("/check", methods=["POST"])
@@ -58,7 +91,7 @@ def check():
     #TODO: add AUTH
     content = request.json
     token = content["token"]
-    claim_code = ClaimCode.from_token(db_session, token)
+    claim_code = ClaimCode.from_token(db.session, token)
     if claim_code:
         return jsonify(claim_code.to_json())
     return abort(404)
@@ -73,14 +106,14 @@ def claim():
     token = content["token"]
     secret = content["secret"]
     address = content["address"]
-    claim_code = ClaimCode.from_token(db_session, token)
+    claim_code = ClaimCode.from_token(db.session, token)
     if claim_code:
         if claim_code.status == "created":
             claim_code.secret = secret
             claim_code.address = address
             claim_code.status = "claimed"
-            db_session.add(claim_code)
-            db_session.commit()
+            db.session.add(claim_code)
+            db.session.commit()
             return jsonify(claim_code.to_json())
         else:
             return abort(400, "already claimed")
@@ -89,6 +122,18 @@ def claim():
 if __name__ == "__main__":
     setup_logging(logging.DEBUG)
 
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    # create tables
+    db.create_all()
+    db.session.commit()
+
+    # process commands
+    print(sys.argv)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'add_user':
+            add_user(sys.argv[2], sys.argv[3])
+        if sys.argv[1] == 'add_role':
+            add_role(sys.argv[2], sys.argv[3])
+    else:
+        # Bind to PORT if defined, otherwise default to 5000.
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
--- models.py
+++ models.py
@@ -1,13 +1,78 @@
-import time
+import datetime
 
-from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
-import sqlalchemy.types as types
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.expression import func
-from sqlalchemy import or_, and_, desc
+from flask import redirect, url_for, request
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required, current_user
+from flask_admin.contrib import sqla
 from marshmallow import Schema, fields
 
-from database import Base
+from app_core import app, db
+
+# Define models
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    @classmethod
+    def from_name(cls, session, name):
+        return session.query(cls).filter(cls.name == name).first()
+
+    def __str__(self):
+        return self.name
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    @classmethod
+    def from_email(cls, session, email):
+        return session.query(cls).filter(cls.email == email).first()
+
+    def __str__(self):
+        return self.email
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+# Create customized model view class
+class RestrictedModelView(sqla.ModelView):
+    can_create = False
+    can_delete = False
+    can_edit = False
+    column_exclude_list = ['password', 'secret']
+
+    def is_accessible(self):
+        return (current_user.is_active and
+                current_user.is_authenticated and
+                current_user.has_role('admin')
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
 
 class ClaimCodeSchema(Schema):
     date = fields.Float()
@@ -16,17 +81,16 @@ class ClaimCodeSchema(Schema):
     address = fields.String()
     status = fields.String()
 
-class ClaimCode(Base):
-    __tablename__ = "claim_codes"
-    id = Column(Integer, primary_key=True)
-    date = Column(Float, nullable=False)
-    token = Column(String, nullable=False, unique=True)
-    secret = Column(String)
-    address = Column(String)
-    status = Column(String)
+class ClaimCode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime())
+    token = db.Column(db.String(255), unique=True, nullable=False)
+    secret = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+    status = db.Column(db.String(255))
 
     def __init__(self, token):
-        self.date = time.time()
+        self.date = datetime.datetime.now()
         self.token = token
         self.secret = None
         self.address = None
