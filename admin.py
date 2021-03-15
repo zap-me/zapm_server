882cec64ed66374b50bf5d27f09e074775157f90
Erick Oliveros <eoliveros@redratclothing.co.nz>
2021-03-15 21:44:03 +0000

    added 4 tables categories, categories_proposals, payment, and proposals the same as zapd. seed table added as well. initial test for sending from invidividual wallets works. Removed teh commented lines i could find. Also still trying to figure out how to change the create_view for the Proposal, wanted it to check to make sure that the Seeds.wallet_seed is not empty.

--- admin.py
+++ admin.py
@@ -3,7 +3,7 @@ import flask_admin
 from flask_admin import helpers as admin_helpers
 
 from app_core import app, db
-from models import security, RestrictedModelView, AdminUserModelView, FinanceUserModelView, BankAdminModelView, BankModelView, ApiKeyModelView, ClaimCodeModelView, TxNotificationModelView, MerchantTxModelView, SettlementAdminModelView, SettlementModelView, Role, User, ClaimCode, TxNotification, ApiKey, MerchantTx, Settlement, Bank
+from models import security, RestrictedModelView, AdminUserModelView, FinanceUserModelView, BankAdminModelView, BankModelView, ApiKeyModelView, ClaimCodeModelView, TxNotificationModelView, MerchantTxModelView, SettlementAdminModelView, SettlementModelView, ProposalAdminModelView, ProposalUserModelView, SeedsUserModelView, SeedsAdminModelView, WavesTxModelView, Role, User, ClaimCode, TxNotification, ApiKey, MerchantTx, Settlement, Bank, Category, Proposal, Seeds, WavesTx
 
 # Create admin
 admin = flask_admin.Admin(
@@ -15,16 +15,22 @@ admin = flask_admin.Admin(
 
 # Add model views
 admin.add_view(RestrictedModelView(Role, db.session, category='Admin'))
+admin.add_view(RestrictedModelView(Category, db.session, category='Admin'))
 admin.add_view(AdminUserModelView(User, db.session, category='Admin', endpoint='user_admin'))
 admin.add_view(FinanceUserModelView(User, db.session, category='Admin', endpoint='user_finance'))
 admin.add_view(BankAdminModelView(Bank, db.session, category='Admin' ))
 admin.add_view(SettlementAdminModelView(Settlement, db.session, category='Admin'))
+admin.add_view(ProposalAdminModelView(Proposal, db.session, category='Admin', endpoint='admin_proposal'))
+admin.add_view(WavesTxModelView(WavesTx, db.session, name='Waves Transactions', category='Admin'))
+admin.add_view(SeedsAdminModelView(Seeds, db.session, endpoint='admin_seed', category='Admin'))
 admin.add_view(ClaimCodeModelView(ClaimCode, db.session, category='Reports'))
 admin.add_view(TxNotificationModelView(TxNotification, db.session, category='Reports'))
 admin.add_view(MerchantTxModelView(MerchantTx, db.session, name='Retail Transactions' ,category='Retailer'))
 admin.add_view(ApiKeyModelView(ApiKey, db.session, category='Retailer'))
 admin.add_view(BankModelView(Bank, db.session, endpoint='BankUser', category='Retailer'))
 admin.add_view(SettlementModelView(Settlement, db.session, endpoint='SettlementUser', category='Retailer'))
+admin.add_view(SeedsUserModelView(Seeds, db.session, endpoint='user_seed', category='Retailer'))
+admin.add_view(ProposalUserModelView(Proposal, db.session, endpoint='user_proposal'))
 
 # define a context processor for merging flask-admin's template context into the
 # flask-security views.
